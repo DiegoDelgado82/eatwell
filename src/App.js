e5bdc926afe67c93bafcbc20a388948a5dcae3bf
Diego Delgado <diegodelgado365@gmail.com>
2025-02-10 10:07:36 -0300

    resolviendo problema de refresco

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import MainLayout from './components/MainLayout';
 import ListaEatwellTable from './components/ListaEatwellTable';
 import VerPedidos from './components/VerPedidos';
@@ -12,6 +12,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Ruta principal con layout */}
         <Route path="/" element={<MainLayout />}>
           <Route index element={<div className="text-center mt-4">Seleccione una opción</div>} />
           <Route path="generar-pedido" element={<ListaEatwellTable />} />
@@ -19,6 +20,9 @@ function App() {
           <Route path="realizar-precios" element={<RealizarPrecios />} />
           <Route path="cargar-lista" element={<CargarLista />} />
         </Route>
+
+        {/* Ruta de "catch-all" para manejar rutas no existentes */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
